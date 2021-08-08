9c2f58758aaf1f79bd7e2b0dae028a40704f1352
Zi Qiang <zqleow@outlook.com>
2021-08-08 18:05:42 +0800

    Updated with line plot
    - Used seaborn line plot to plot the increase of cases by months in Singapore for 2021
    - Use formatter so that only months are displayed on x-axis instead of days

--- Section 4/data-visualization/covid19-visualization.py
+++ Section 4/data-visualization/covid19-visualization.py
@@ -4,30 +4,22 @@ import requests
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 
 api_url = "https://api.covid19api.com/total/country/singapore/status/confirmed?from=2021-01-01T00:00:00Z&to=2021-08-08T00:00:00Z"
 response = requests.get(api_url)
-
-# responseBody = response.json()
-
 covidCases = json.loads(response.text)
-# print(covidCases)
 
 caseNumList = []
 dateConfirmedList = []
 
 
 for case in covidCases:
-    # print(case)
-
-    # print(case["Cases"])
-
     caseNumList.append(case["Cases"])
 
-
     dateConfirmed = re.search(r'\d{4}-\d{2}-\d{2}', case["Date"])
-    # print(dateConfirmed.group())
+
     dateConfirmedList.append(dateConfirmed.group())
 
 
@@ -35,16 +27,30 @@ print(caseNumList)
 print(dateConfirmedList)
 
 dict = {'caseNum': caseNumList, 'dateConfirmed': dateConfirmedList}
-df = pd.DataFrame(dict)
-# df.dateConfirmed = df.dateConfirmed.squeeze()
+df = pd.DataFrame(data=dict, columns=['caseNum', 'dateConfirmed'])
+
+# Convert date format to simplified (MMM DD)
+dt = pd.to_datetime(df['dateConfirmed'])
+dt = dt.apply(lambda x: x.strftime('%b %d'))
+print(dt)
+df['dateConfirmed'] = dt
 print(df)
 
-chart = sns.lineplot(x = 'dateConfirmed', y = 'caseNum', data=df)
+
+chart = sns.lineplot(x='dateConfirmed', y='caseNum', data=df).set_title(
+    'Trend of increase in number of COVID-19 cases in Singapore by months (2021)')
 # chart = sns.lineplot(data=df)
-# plt.xlabel("Date of Confirmation")
-# plt.ylabel("Case Number")
-# plt.plot('caseNum', 'dateConfirmed', data=df)
+plt.xlabel("Period")
+plt.ylabel("Case Number")
+
+X = plt.gca().xaxis
+locator = mdates.MonthLocator()  # every month
+# Specify the format - %b gives us Jan, Feb...
+fmt = mdates.DateFormatter('%b')
+
+# Adjust x axis to display just months
+X.set_major_locator(locator)
+# Specify formatter
+X.set_major_formatter(fmt)
+
 plt.show()
-
-
-# plt.show()
